2204ca291aa2acf2888e1c6477a6ce632cd0fb08
mohaimen <mohaimen.jatri@gmail.com>
2023-08-01 01:32:47 +0600

    component load with asynchronous loading

--- react/product-details-page/src/App.jsx
+++ react/product-details-page/src/App.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, {Suspense} from "react";
 import ReactDOM from "react-dom";
-import Header from "home/Header";
+//Asynchronous loading component
+const Header = React.lazy(() => import("home/Header"));
 import Footer from "home/Footer";
 import "./index.scss";
 
 const App = () => (
     <div className="text-3xl mx-auto max-w-6xl">
-        <Header title="Header from Product Details page" />
+        <Suspense fallback={<div>Loading...</div>}>
+            <Header title="Header from Product Details page" />
+        </Suspense>
         <div className="py-10 bg-gray-300 h-80">
             <div className="text-center">Home Page From Product details page</div>
         </div>
